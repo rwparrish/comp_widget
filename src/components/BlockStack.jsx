a9f877453ec65b6385fd5cd1835d42fcaf2e3f83
rwparrish <60946750+rwparrish@users.noreply.github.com>
2025-01-13 11:15:40 -0700

    render blocks as test

--- src/components/BlockStack.jsx
+++ src/components/BlockStack.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import Block from './Block';
 import './BlockStack.css';
+
+const COLORS = [
+    '#FF6B6B',
+    '#4ECDC4',
+    '#45B7D1',
+    '#96CEB4',
+    '#FFEEAD'
+  ];
 
 const BlockStack = ({ position }) => {
   return (
     <div className={`block-stack ${position}-stack`}>
-        BLOCK STACK
-      {/* Block stack content will go here */}
+       <div className="cubes-container">
+        {COLORS.map((color, index) => (
+          <Block key={index} color={color} />
+        ))}
+      </div>
     </div>
   );
 };
